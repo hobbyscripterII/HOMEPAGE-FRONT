f3c562b56eb9dc711ec346188213d03003f1c762
이주영 <wruomma2@naver.com>
2025-01-28 17:07:19 +0900

    feat: 로그인 페이지 추가

---
+++ js/cmmn.js
@@ -0,0 +1,31 @@
+$(function () {
+  let currentUrl = window.location.href;
+  
+  $('nav a').each((idx, item) => {
+    let linkUrl = $(item).attr('href');
+
+    if(currentUrl.includes(linkUrl) && linkUrl != '') {
+      $(item).addClass('active');
+    }
+  });
+
+  $(window).scroll(function () {
+    let winTop = $(window).scrollTop();
+    
+    if (winTop >= 200) {
+      $("body").addClass("sticky-header");
+    } else {
+      $("body").removeClass("sticky-header");
+    }
+  });
+})
+
+function formattedDate(str) {
+  let date = new Date(str);
+  let year = date.getFullYear();
+  let month = date.getMonth() + 1;
+  let day = date.getDate();
+  let result = `${year}.${month}.${day}`;
+
+  return result;
+}
